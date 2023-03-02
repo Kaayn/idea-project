0f3a7aa55f6a6ef295d6ee0a54a29fac413cc531
YannBonaudo <yannbonaudo@yahoo.fr>
2023-03-02 15:04:00 +0100

    feat(bo): save user in context

--- backoffice/src/components/loginForm/index.tsx
+++ backoffice/src/components/loginForm/index.tsx
@@ -1,15 +1,26 @@
 import { useForm } from "react-hook-form";
 import { login } from "../../utils/firebaseInit"
 import { useNavigate } from "react-router-dom";
+import { AuthDataContext, ContextAction } from "../../App";
+import { useContext } from "react";
+import { User } from "@firebase/auth";
 
 export const LoginForm = () => {
     
   const { register, handleSubmit, formState: { errors } } = useForm();
 
+  const { dispatchData } = useContext(AuthDataContext)
+
   const onSubmit = async (data: any) => {
     const user = await login(data.Email, data.Password);
     
-    if ("accessToken" in user) navigate("/home")
+    if ("accessToken" in user) {
+      dispatchData({
+        type: ContextAction.setUser,
+        newValue: user as User,
+      })
+      navigate("/home")
+    }     
   };
     
   const navigate = useNavigate();
