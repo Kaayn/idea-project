1b1e80593e7072d5f5b3014de30e8b4b1d878c72
Kaayn <leo.dimartinopro@gmail.com>
2023-03-03 10:13:59 +0100

    clean rebase

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,9 @@
-<<<<<<< HEAD
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-=======
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
->>>>>>> d37dd84... feat(front): All Front
 import "./App.css";
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-=======
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
@@ -30,5 +14,4 @@ const App = () => {
 //TODO: Panier
 //TODO: Sauvegarder la commande dans firebase
 
->>>>>>> d37dd84... feat(front): All Front
 export default App;
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-=======
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
@@ -19,5 +7,4 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
->>>>>>> d37dd84... feat(front): All Front
 );
