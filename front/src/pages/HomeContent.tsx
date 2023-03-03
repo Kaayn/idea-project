2b383f4847d513ba42f5de9d5d6c5ef44c504b6b
Kaayn <leo.dimartinopro@gmail.com>
2023-03-03 14:15:11 +0100

    jpp

--- front/src/pages/HomeContent.tsx
+++ front/src/pages/HomeContent.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from "react";
-import Header from "../components/header";
+import Header from "../components/Header";
 import ProductList from "../components/ProductList";
 import { ProductsContext } from "../context/product";
 import CartModal from "../components/CartModal";
